c8a1162fc0fc905e6084efb719e02a1c1d620bb0
Kevin Swiber <kswiber@gmail.com>
2013-10-10 14:58:37 -0400

    Moving Usergrid session for calypso into its own module.

--- resources/index.js
+++ resources/index.js
@@ -3,13 +3,14 @@ var calypso = require('calypso');
 var mappings = require('../persistence/mappings');
 var Product = require('../models/product');
 var ProductsResource = require('./products_resource');
+var UsergridSession = require('calypso-usergrid');
 
 var options = {
-  org: process.env.USERGRID_ORG_NAME || 'cosafinity',
-  app: process.env.USERGRID_APP_NAME || 'sandbox'
+  orgName: process.env.USERGRID_ORG_NAME || 'cosafinity',
+  appName: process.env.USERGRID_APP_NAME || 'sandbox'
 };
 
-var session = calypso.UsergridSession.create(options, function(config) {
+var session = UsergridSession.create(options, function(config) {
   config.add(mappings);
 });
 
